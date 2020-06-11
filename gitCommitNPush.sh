775e5d070c743b664825be948761697858e675de
ChristopherJEkd <christopherj.ekd@gmail.com>
2020-06-12 02:20:56 +0530

    Auto Commit N Push

--- gitCommitNPush.sh
+++ gitCommitNPush.sh
@@ -64,6 +64,20 @@
 
 
 
+#~ # Updates were rejected because the remote contains work that you do
+#~ # SOLUTION #
+#~ git remote add origin https://github.com/ChristopherJEkd/.batteryNotify.git
+#~ # pull those changes
+#~ git pull origin master
+#~ # or optionally, 'git pull origin master --allow-unrelated-histories' 
+#~ # if you have initialized repo in github and also committed locally
+#~ #
+#~ # now, push your work to your new repo
+#~ git push origin master
+
+
+
+
 # Script Starts Here ###########################################################
 #~ 
 
