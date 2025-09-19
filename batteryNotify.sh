f5f666a4594f7cedf075d533a0e04761411f4b38
Christopher Johnson <49064722+ChristopherJEkd@users.noreply.github.com>
2025-09-20 04:56:33 +0530

    Full Charge in Silent Mode
    This is for business laptops

--- batteryNotify.sh
+++ batteryNotify.sh
@@ -9,18 +9,22 @@
 while true
 do
 	export DISPLAY=:0.0
+	current_directory=$(pwd)
+	img_path="${current_directory}/battery.png"
+	#~ /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
+	audio_path="/usr/share/sounds/gnome/default/alerts/click.ogg"
 	batteryPercentMultiLine=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 	battery_percent=`echo "${batteryPercentMultiLine}" | head -1`
     
 	if on_ac_power; then
-		if [ "$battery_percent" -gt 95 ]; then
-			notify-send -i "$PWD/battery.png" "Discharge Battery Now" "Level: ${battery_percent}% "
-			paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
-			#~ /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
-		fi
+		: # Do nothing
+		#~ if [ "$battery_percent" -gt 95 ]; then
+			#~ notify-send -i $img_path "Discharge Battery Now" "Level: ${battery_percent}% "
+			#~ paplay $audio_path
+		#~ fi
 	elif [ "$battery_percent" -le 70 ]; then
-		notify-send -i "$PWD/battery.png" "Recharge Battery Now" "Level: ${battery_percent}% "
-		paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+		notify-send -i $img_path "Recharge Battery Now" "Level: ${battery_percent}% "
+		paplay $audio_path
     fi
 
     sleep 300 # (5 minutes)
